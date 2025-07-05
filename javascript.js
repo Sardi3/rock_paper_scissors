470788c78fb04df0ca554af2e54469c3f117aadb
Rexo <207926947+Sardi3@users.noreply.github.com>
2025-07-05 20:20:59 +0100

    Add index.html and javascript.js with functions for computer and user choices

---
+++ javascript.js
@@ -0,0 +1,15 @@
+function getComputerChoice(){
+    let num = Math.floor(Math.random()*3);
+    if(num==0){
+        return 'rock';
+    } else if(num==1){
+        return 'paper';
+    } else{
+        return 'scissors'
+    };
+};
+
+function getHumanChoice(){
+    let escolha = prompt('Rock, Paper, Scissors?');
+    return escolha.toLowerCase();
+}
